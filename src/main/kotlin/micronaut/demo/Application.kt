801a6dff9bab14165a39a81b75ce03f783c101a9
Kevin Tholen <kevin.tholen@favordelivery.com>
2020-09-13 20:46:30 -0500

    openapi

--- src/main/kotlin/micronaut/demo/Application.kt
+++ src/main/kotlin/micronaut/demo/Application.kt
@@ -1,11 +1,17 @@
 package micronaut.demo
 
 import io.micronaut.runtime.Micronaut.build
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
 
-fun main(args: Array<String>) {
-	build()
-		.args(*args)
-		.packages("micronaut.demo")
-		.start()
+@OpenAPIDefinition
+object Application {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        build()
+            .args(*args)
+            .packages("micronaut.demo")
+            .start()
+    }
 }
 
